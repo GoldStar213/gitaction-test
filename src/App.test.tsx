11d05ce6c1ad74ba48f00acbfc4c027e47169813
GoldStar213 <seniordev213@gmail.com>
2024-02-28 18:45:46 +0900

    updated test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/JSON Data/i);
   expect(linkElement).toBeInTheDocument();
 });
